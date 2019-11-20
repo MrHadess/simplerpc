0053a6af21774319e3a84ff9e99eefd128a93380
mrhades <779628128@qq.com>
2019-11-20 15:28:52 +0800

    1.Update version 2.Update README.md

--- src/main/java/com/mh/simplerpc/ServiceManager.java
+++ src/main/java/com/mh/simplerpc/ServiceManager.java
@@ -34,7 +34,7 @@ public class ServiceManager {
     private ProtocolCore protocolCore;
 
     public void startup() {
-        logger.info("SimpleRPC startup (version:1.0.8)");
+        logger.info("SimpleRPC startup (version:1.1.0)");
         communicationManager.startup();
 
 
