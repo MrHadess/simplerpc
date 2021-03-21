755b5e7d6f9ad9588df75f92c2bbaa708506cae2
MrHadess <779628128@qq.com>
2021-03-21 15:28:43 +0800

    CacheToClasses optimize performance

--- core/src/main/java/com/mh/simplerpc/service/protocol/CacheToClasses.java
+++ core/src/main/java/com/mh/simplerpc/service/protocol/CacheToClasses.java
@@ -10,33 +10,114 @@ package com.mh.simplerpc.service.protocol;
 
 import com.mh.simplerpc.SimpleRPCServiceApplication;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CacheToClasses {
 
     // Will be load to JVM stack memory
+    private static final ConcurrentHashMap.KeySetView<String,Boolean> processMap = ConcurrentHashMap.newKeySet();
+
+    private static final Map<String,AtomicInteger> processMapp = new ConcurrentHashMap<>();
+    // state
 
     /*
     * map model class name to class
     *
     * */
-    private static HashMap<String,Class<?>> classesMap = new HashMap<String, Class<?>>();
+    private static final Map<String,Class<?>> classesMap = new ConcurrentHashMap<>();
 
     public static Class<?> stringToClass(String className) throws ClassNotFoundException {
 
         Class<?> matchClass = classesMap.get(className);
         if (matchClass != null) return matchClass;
-        Class<?> forNameClass;
-        synchronized (CacheToClasses.class) {
-            if (SimpleRPCServiceApplication.isStartCommand()) {
-                forNameClass = SimpleRPCServiceApplication.getCommandClassLoader().loadClass(className);
-            } else {
-                forNameClass = Class.forName(className);
+
+        AtomicInteger num = processMapp.putIfAbsent(className,new AtomicInteger(0));
+        if (num == null) {
+            System.out.println(String.format("Thread name %s",Thread.currentThread().getName()));
+            Class<?> forNameClass;
+            try {
+                if (SimpleRPCServiceApplication.isStartCommand()) {
+                    forNameClass = SimpleRPCServiceApplication.getCommandClassLoader().loadClass(className);
+                } else {
+                    forNameClass = Class.forName(className);
+                }
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            } catch (ClassNotFoundException e) {
+                int nowValue = processMapp.get(className).decrementAndGet();
+                if (nowValue < 0) {
+                    processMap.remove(className);
+                }
+                throw e;
             }
-            classesMap.put(className,forNameClass);
+
+            // check again key
+            if (!classesMap.containsKey(className)) {
+                classesMap.put(className,forNameClass);
+            }
+
+            processMapp.get(className).decrementAndGet();
+            return forNameClass;
         }
 
-        return forNameClass;
+
+//        int processOrderNum = num.incrementAndGet();
+        int processOrderNum = num.getAndIncrement();
+        AtomicInteger tempNumAtomic;
+        while ((tempNumAtomic = processMapp.get(className)) != null && tempNumAtomic.get() > processOrderNum) {
+            System.out.println(String.format("Thread %s wait",Thread.currentThread().getName()));
+        }
+
+        // unlock next
+        int nowValue = num.decrementAndGet();
+        if (nowValue < 0) {
+            processMap.remove(className);
+        }
+        matchClass = classesMap.get(className);
+        if (matchClass == null) {
+            throw new ClassNotFoundException(className);
+        }
+        return matchClass;
+
+//        if (processMap.contains(className)) {
+//            Class<?> forNameClass;
+//            try {
+//                if (SimpleRPCServiceApplication.isStartCommand()) {
+//                    forNameClass = SimpleRPCServiceApplication.getCommandClassLoader().loadClass(className);
+//                } else {
+//                    forNameClass = Class.forName(className);
+//                }
+//            } catch (ClassNotFoundException e) {
+//                processMap.remove(className);
+//                throw e;
+//            }
+//
+//            // check again key
+//            if (!classesMap.containsKey(className)) {
+//                classesMap.put(className,forNameClass);
+//            }
+//        }
+
+//        Class<?> forNameClass;
+//        if (SimpleRPCServiceApplication.isStartCommand()) {
+//            forNameClass = SimpleRPCServiceApplication.getCommandClassLoader().loadClass(className);
+//        } else {
+//            forNameClass = Class.forName(className);
+//        }
+//
+//        // check again key
+//        if (!classesMap.containsKey(className)) {
+//            classesMap.put(className,forNameClass);
+//        }
+
+//        return forNameClass;
+//        return classesMap.get(className);
     }
 
 
